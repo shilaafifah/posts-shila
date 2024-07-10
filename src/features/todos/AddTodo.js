c767f865712f5341cc7a38f7d1289e796ae9f84d
Shila <shilanurafifah@gmail.com>
2024-07-10 13:00:30 +0700

    (feat)/add-to-do-post

---
+++ src/features/todos/AddTodo.js
@@ -0,0 +1,45 @@
+import { useState } from "react"
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from "react-redux"
+import { nanoid } from "@reduxjs/toolkit"
+import { addTodo } from "./todoSlice"
+import { Button, TextField, FormContainer } from "../../components"
+
+const AddTodo = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const [values, setValues] = useState({
+    title: '',
+    details: ''
+  })
+
+  const handleAddTodo = () => {
+    setValues({ title: '', details: ''})
+    console.log(values);
+    dispatch(addTodo({
+      id: nanoid(8),
+      title: values.title,
+      details: values.details
+    }))
+    navigate('/')
+  }
+
+  return (
+    <FormContainer title="Add Todo">
+      <TextField
+        label="Todo"
+        value={values.title}
+        onChange={(e) => setValues({ ...values, title: e.target.value})}
+        inputProps={{ type: 'text', placeholder: 'Todo Item Title'}}
+      />
+      <TextField
+        label="Details"
+        value={values.details}
+        onChange={(e) => setValues({ ...values, details: e.target.value})}
+        inputProps={{ type: 'text', placeholder: 'Todo item details...'}}
+      />
+      <Button onClick={handleAddTodo}>Create</Button>
+    </FormContainer>
+  )
+}
+export default AddTodo
